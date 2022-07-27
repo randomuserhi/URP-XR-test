7612bbe11630240e698da14cdb0a6273d997aa24
randomuserhi <magicalworld.nano@gmail.com>
2022-07-27 11:27:42 +0100

    Created Mixed build for both oculus and hololens

--- HololensTest2/Il2CppOutputProject/Source/lumpedcpp/Lump_libil2cpp_codegen.cpp
+++
@@ -1,4 +0,0 @@
-#include "il2cpp-config.h"
-#include "C:\Users\Bboy1\Documents\Work\Internship\Builds\HololensTest2\Il2CppOutputProject\IL2CPP\libil2cpp\codegen\il2cpp-codegen-common.cpp"
-#include "C:\Users\Bboy1\Documents\Work\Internship\Builds\HololensTest2\Il2CppOutputProject\IL2CPP\libil2cpp\codegen\il2cpp-codegen-il2cpp.cpp"
-#include "C:\Users\Bboy1\Documents\Work\Internship\Builds\HololensTest2\Il2CppOutputProject\IL2CPP\libil2cpp\codegen\il2cpp-codegen-tiny.cpp"
